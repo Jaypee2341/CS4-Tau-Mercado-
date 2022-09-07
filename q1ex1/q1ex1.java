69d90232988aebd5b065530e29ddfc81a461e355
Jaypee2341 <96430344+Jaypee2341@users.noreply.github.com>
2022-09-07 09:02:12 +0800

    q1ex1

---
+++ q1ex1/q1ex1.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joji.supremacy;
+        import java.util.Scanner
+/**
+ *
+ * @author TAU
+ */
+public class JojiSupremacy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //data types
+            int age = 69;
+            double money = 69.42;
+            boolean mayJowa = false;
+            char section = 'T';
+            String jp = "sad";
+        //operators
+            int sum = 3+4;
+            double div = 3/2.0;
+            //assignment operators
+            //+=, -=, /=, %=
+            //comparison operators (evaluates into boolean)
+            //logic operators (&&, !!, !)
+            //Ternary Moment
+        //control flow statements:
+            //if()
+                if(mayJowa){
+                    jp = "happy";
+                }
+                else{
+                   jp = "sad";
+                }
+        
+            //switch case
+                switch(day){
+                    case "monday":
+                        Monday;
+                    case "tuesday":
+                        Tuesday;
+                    break;
+                }
+        
+            //loops
+                //while
+                    while(!isDone){
+            
+                    }
+                //do while
+                    do{
+                
+                    }while(!isDone)
+                //for
+                for(int i = 0; i<10; i+=2){
+                }
+                //break&continue
+                if(count == 1){
+                    break;
+                    continue;
+                }
+        //I/O statements
+            //print
+                system.out.print();
+                println("My name is" + name + ".");
+                printf("My name is %s. My job is %s. My grade is %2f");
+            //Scanner
+                Scanner sc = new Scanner(System in);
+                System.out.print("What is your name?");
+                String name = "sc.nextline()";
+                System.out.print("Hello, %s");
+                int age = Integer.parseInt(sc.nextLine());
+    }
+                    
+    }
+    
+}
