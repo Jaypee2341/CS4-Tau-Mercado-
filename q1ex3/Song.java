977395ba8a94e776b8e85be31cf35deaa97c6600
Jaypee2341 <96430344+Jaypee2341@users.noreply.github.com>
2022-09-28 00:29:01 +0800

    Update Song.java

---
+++ q1ex3/Song.java
@@ -0,0 +1,11 @@
+public class Song{
+  String title;
+  String artist;
+  String genre;
+  
+  public Song(String t, String a, String g){
+        title = t;
+        artist = a;
+        genre = g;
+  }
+}
